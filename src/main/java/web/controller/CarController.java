9135369b26cf8ea02a07c3b9b7fa892a6099e15e
DenisLeontev <ldb107@mail.ru>
2022-11-14 18:25:16 +0500

    5 commit

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,12 +1,14 @@
 package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
 import web.service.CarServiceImp;
 
+@Controller
 public class CarController {
 
     private final CarService carService;
@@ -16,9 +18,11 @@ public class CarController {
         this.carService = carServiceImp;
     }
 
-    @GetMapping(value = "cars")
+    @GetMapping(value = "/cars")
     public String printCarsParam(@RequestParam(value = "count", defaultValue = "5") int amount, ModelMap model) {
         model.addAttribute("cars", carService.getAmountCars(amount));
         return "cars";
     }
+
+
 }
